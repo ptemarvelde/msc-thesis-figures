8e38208b13f977356fde72796a09ff86625287cf
Pepijn te Marvelde <ptemarvelde@gmail.com>
2024-03-04 12:14:10 +0100

    updates

--- figure_utils.py
+++ figure_utils.py
@@ -3,6 +3,11 @@ RUNTIME_DIR = "/home/pepijn/Documents/uni/y5/thesis/amalur/amalur-experiments/re
 FIGURE_WIDTH_INCHES = 8.0
 MODEL_OPERATORS = ["KMeans", "Logistic Regression", "Linear Regression", "Gaussian"]
 
+import sys
+# sys.path.append(f"{RUNTIME_DIR}/src")
+sys.path.append("/home/pepijn/Documents/uni/y5/thesis/amalur/amalur-experiments/results/full_1/src/")
+
+from util import read_data_chars
 
 def save_table(table, path):
     print(table)
